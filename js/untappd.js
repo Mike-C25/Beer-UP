986aee87395bc121ecb6e2c23e3ade5c33fd6f9d
vin <vrp908@gmail.com>
2017-09-11 19:48:01 -0400

    init vin

---
+++ js/untappd.js
@@ -0,0 +1,11 @@
+
+
+var clientID = "client_id=884D240A42BC899E9117BEE3F75DD6E74B7BBB79"
+var clientSecret ="client_secret=8270A9AC633784F8A2CF46F7DC17F16CA162571A"
+var beerType  = ""
+var queryURL  = "https://api.untappd.com/v4/search/beer?q=" + beerType + "&" + clientID + "&" + clientSecret; 
+              
+
+
+
+
