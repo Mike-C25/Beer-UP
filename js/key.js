9948e24295b0e8a22eb465624a3cfe0a17be4ee1
vin <vrp908@gmail.com>
2017-09-11 20:19:31 -0400

    vin 2

--- js/key.js
+++ js/key.js
@@ -1,7 +1,8 @@
 
+var keys = {
+ clientID: "client_id=884D240A42BC899E9117BEE3F75DD6E74B7BBB79";
+ clientSecret: "client_secret=8270A9AC633784F8A2CF46F7DC17F16CA162571A";
+   }
 
-someapikey = 123123;
 
-
-
-module.exports = someapikey;
+module.exports = keys;
--- js/untappd.js
+++ js/untappd.js
@@ -1,9 +1,19 @@
 
+$(document).ready(function(){
+	
+// var keys = require('./key.js');
+var key = require('./key.js');
+console.log(keys);
+	console.log("hi");
+	var clientID = "client_id=" + keys.clientID;
+	var clientSecret ="client_secret=" + keys.clientSecret;
 
-var clientID = "client_id=884D240A42BC899E9117BEE3F75DD6E74B7BBB79"
-var clientSecret ="client_secret=8270A9AC633784F8A2CF46F7DC17F16CA162571A"
-var beerType  = ""
-var queryURL  = "https://api.untappd.com/v4/search/beer?q=" + beerType + "&" + clientID + "&" + clientSecret; 
+	console.log(clientID , clientSecret);
+	var beerType  = ""
+	var queryURL  = "https://api.untappd.com/v4/search/beer?q=" + beerType + "&" + clientID + "&" + clientSecret; 
+});
+
+
               
 
 
