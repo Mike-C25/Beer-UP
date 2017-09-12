bb3c2dacd1f56ece5fab45530cead7dd21afee4f
Michael Chiang <michael.chiang25@gmail.com>
2017-09-12 08:42:00 -0400

    git ignore

--- js/untappd.js
+++ js/untappd.js
@@ -1,51 +1,48 @@
-	// var keys = {}
-	// requirejs(["./js/key.js"], function(key) {
-	//     console.log(key.clientID);
-	//     keys.clientID = key.clientID;
-	//     keys.clientSecret = key.clientSecret;
-	// });
-	$(document).ready(function() {
+require(['domReady!'], function(doc) {
+    var keys = {}
+    requirejs(["./js/key.js"], function(key) {
+        console.log(key);
+        // keys.clientID = key.clientID;
+        // keys.clientSecret = key.clientSecret;
+    });
+    var masterBeer = {
+        checkinCount: [],
+        beerName: [],
+        beerInfo: [],
+        beerID: [],
+        brewery: []
+    };
 
-	    var masterBeer = {
-	        checkinCount: [],
-	        beerName: [],
-	        beerInfo: [],
-	        beerID: [],
-	        brewery: []
-	    };
+    // console.log(keys);
 
-	    // console.log(keys);
+    // var clientID = keys.clientID;
+    // var clientSecret = keys.clientSecret;
 
-	    // var clientID = keys.clientID;
-	    // var clientSecret = keys.clientSecret;
+    // console.log(clientID, clientSecret);
+    // var beerType = ""
+    // var queryURL = "https://api.untappd.com/v4/search/beer?q=" + beerType + "&" + clientID + "&" + clientSecret;
+    // var clientID = "client_id=884D240A42BC899E9117BEE3F75DD6E74B7BBB79";
+    // var clientSecret = "client_secret=8270A9AC633784F8A2CF46F7DC17F16CA162571A";
 
-	    // console.log(clientID, clientSecret);
-	    // var beerType = ""
-	    // var queryURL = "https://api.untappd.com/v4/search/beer?q=" + beerType + "&" + clientID + "&" + clientSecret;
-	    // var clientID = "client_id=884D240A42BC899E9117BEE3F75DD6E74B7BBB79";
-	    // var clientSecret = "client_secret=8270A9AC633784F8A2CF46F7DC17F16CA162571A";
+    // console.log(clientID, clientSecret);
+    var beerType = "Stout"
+    var queryURL = "https://api.untappd.com/v4/search/beer?q=" + beerType + "&" + "limit=50&" + clientID + "&" + clientSecret;
 
-	    console.log(clientID, clientSecret);
-	    var beerType = "Stout"
-	    var queryURL = "https://api.untappd.com/v4/search/beer?q=" + beerType + "&" + "limit=50&" + clientID + "&" + clientSecret;
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).done(function(result) {
 
-	    console.log(queryURL);
-	    $.ajax({
-	        url: queryURL,
-	        method: 'GET',
-	    }).done(function(result) {
+        console.log(result);
+        for (var i = 0; i < result.response.beers.count; i++) {
+            masterBeer.checkinCount.push(result.response.beers.items[i].checkin_count);
+            masterBeer.beerName.push(result.response.beers.items[i].beer.beer_name);
+            masterBeer.beerInfo.push(result.response.beers.items[i].beer.beer_description);
+            masterBeer.beerID.push(result.response.beers.items[i].beer.bid);
+            masterBeer.brewery.push(result.response.beers.items[i].brewery.brewery_name);
+        }
+        console.log(masterBeer);
 
-	        console.log(result);
-	        for (var i = 0; i < result.response.beers.count; i++) {
-	            masterBeer.checkinCount.push(result.response.beers.items[i].checkin_count);
-	            masterBeer.beerName.push(result.response.beers.items[i].beer.beer_name);
-	            masterBeer.beerInfo.push(result.response.beers.items[i].beer.beer_description);
-	            masterBeer.beerID.push(result.response.beers.items[i].beer.bid);
-	            masterBeer.brewery.push(result.response.beers.items[i].brewery.brewery_name);
-	        }
-	        console.log(masterBeer);
-
-	    });
-
-
-	});
+    });
+});
