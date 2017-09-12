c051409d5a6b3e13729789c066113ea399d2c8b0
Michael Chiang <michael.chiang25@gmail.com>
2017-09-11 21:34:49 -0400

    Added Keys

---
+++ js/key.sample.js
@@ -0,0 +1,6 @@
+define(function() {
+    return {
+    	clientID: "client_id=497A82JNFABI2841C48JO10S9M64KP25J9ADIOHWEIU94",
+    	clientSecret: "client_secret=0947A0NAOJAMWN124IOANJR1HK1NMASIEUQ"
+	}
+});
