d29171e47ec605b1efa350756150d2ee01322e04
vin <vrp908@gmail.com>
2017-09-18 20:03:14 -0400

    lkansdlkan

--- js/maps.js
+++ js/maps.js
@@ -1,6 +1,16 @@
   
 $(document).ready(function() {
 	console.log(Bar);
+    
+	function initMap() {
+       	var map;
+       map = new google.maps.Map(document.getElementById('map'), {
+         center: uluru,
+         zoom: 8
+        });
+        console.log("done");
+        console.log(map);
+     }
 
     // var mapsKey = "AIzaSyA8a8x-3TdPvdkvSQ94_RgPPyU5kkuaJnM";
     
@@ -8,20 +18,23 @@ $(document).ready(function() {
      
        var iteration = $(this).attr("data-id")
        console.log(iteration)
+
+
        var latitude = Bar[iteration].location.lat;
        var longitude = Bar[iteration].location.lng;
+
+          uluru = { lat: latitude,
+                   lng: longitude
+                  }; 
 
        console.log(latitude);
        console.log(longitude);
 
-       var map;
-       function initMap() {
-       map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: parseInt(latitude), lng: parseInt(longitude)},
-         zoom: 8
-        });
-     }
-       console.log(map);
+       initMap();
+
+       
+
+      
        
     });
 });
