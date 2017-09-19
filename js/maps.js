14657d2da6e6ed08271b7491f456657b35914147
Michael Chiang <michael.chiang25@gmail.com>
2017-09-18 21:18:04 -0400

    Fixed Google Maps scripts

--- js/maps.js
+++ js/maps.js
@@ -1,58 +1,66 @@
-  
+var marker;
 function initMap() {
-  console.log(uluru);
-        var map;
-       map = new google.maps.Map(document.getElementById('map'), {
-         center: uluru,
-         zoom: 8
-        });
-        console.log("done");
-        console.log(map);
-     
-        var marker;
-        marker = new google.maps.Marker({
+    google.maps.visualRefresh = true;
+    console.log(uluru);
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: uluru,
+        zoom: 12
+    });
+    console.log("done");
+    console.log(map);
+
+    marker = new google.maps.Marker({
         position: uluru,
         map: map
-        });
-
-     };
-
-$(document).ready(function() {
-	// console.log(Bar);
-  $(".map").hide();
-
-    // var mapsKey = "AIzaSyA8a8x-3TdPvdkvSQ94_RgPPyU5kkuaJnM";
-    
-    $(document).on("click",'.cards', function() {
-       var iteration = $(this).attr("data-id")
-       console.log(iteration)
-
-
-       var latitude = Bar[iteration].location.lat;
-       var longitude = Bar[iteration].location.lng;
-
-          uluru = { lat: latitude,
-                   lng: longitude
-                  }; 
-
-       // console.log(latitude);
-       // console.log(longitude);
-
-       initMap();
-       $(".results").fadeOut();
-       $(".map").fadeIn(1000);
-
-
-       
-
-      
-       
     });
 
-    $(document).on("click",'.backButt', function() {
 
-      $(".results").fadeIn(1000);
-       $(".map").fadeOut();
-       $("#map").empty()
-     });
+};
+
+
+
+$(document).ready(function() {
+    // console.log(Bar);
+    $(".map").hide();
+
+
+});
+
+$(document).on("click", '.backButt', function() {
+    google.maps.visualRefresh = true;
+    google.maps.event.trigger(map, 'resize');
+    marker.setMap(null);
+    $(".results").fadeIn(1000);
+    $(".map").fadeTo(.0001);
+    $("#map").empty();
+});
+
+
+
+$(document).on("click", '.cards', function() {
+    var iteration = $(this).attr("data-id")
+    console.log(iteration)
+
+
+    var latitude = Bar[iteration].location.lat;
+    var longitude = Bar[iteration].location.lng;
+
+    uluru = {
+        lat: latitude,
+        lng: longitude
+    };
+
+    // console.log(latitude);
+    // console.log(longitude);
+
+    initMap();
+    $(".results").fadeOut();
+    $(".map").fadeIn(1000);
+
+
+
+
+
+
 });
